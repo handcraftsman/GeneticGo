c8b05b56c63d66bbe534474133786dc381dfacd1
Clinton Sheppard <gar3ts@gmail.com>
2012-04-01 21:57:38 -0500

    go fmt updates

--- samples/string_duplication.go
+++ samples/string_duplication.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"fmt";
-	"time";
 	"../genetic"
+	"fmt"
+	"time"
 )
 
 func main() {
 	const genes = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
 	target := "Not all those who wander are lost."
-	calc := func (current string) int {
+	calc := func(current string) int {
 		return calculate(target, current)
 	}
-	
+
 	start := time.Now()
-	
-	disp := func (current string) {
+
+	disp := func(current string) {
 		fmt.Print(current)
 		fmt.Print("\t")
 		fmt.Print(calc(current))
 		fmt.Print("\t")
 		fmt.Println(time.Since(start))
 	}
-	
+
 	var solver = new(genetic.Solver)
 	solver.MaxSecondsToRunWithoutImprovement = 1
-	
+
 	var best = solver.GetBest(calc, disp, genes, len(target), 1)
 	println(best)
-	
+
 	fmt.Print("Total time: ")
 	fmt.Println(time.Since(start))
 }
