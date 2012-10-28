c61223377d379db2097ac18fb43e3aef6047ab19
NightExcessive <nightexcessive@gmail.com>
2012-10-28 15:51:47 -0300

    Fixed an inconsistency in the documentation

--- doc.go
+++ doc.go
@@ -39,8 +39,8 @@
 // 	geneSet := "abc123..." // you decide the set of valid genes
 // 	numberOfGenesInAChromosome := 1 // you decide
 // 	
-// solver.NumberOfConcurrentEvolvers = 4 // you decide, defaults to 1
-// solver.MaxProcs // you decide, defaults to 1
+//  solver.NumberOfConcurrentEvolvers = 4 // you decide, defaults to 1
+//  solver.MaxProcs // you decide, defaults to 1
 //
 // if your problem can be solved with a fixed number of genes:
 // 
